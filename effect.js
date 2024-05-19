d7c5d87dbaa0d2deab4000e8cad4d6475b44a40a
kesumaaa <168466926+jenowleee@users.noreply.github.com>
2024-05-19 22:27:29 +0800

    Create effect.js

---
+++ effect.js
@@ -0,0 +1,8 @@
+$(window).load(function(){
+	$('.loading').fadeOut('fast');
+	$('.container').fadeIn('fast');
+});
+$('document').ready(function(){
+		var vw;
+		$(window).resize(function(){
+			 vw = $(window).width()/2;
